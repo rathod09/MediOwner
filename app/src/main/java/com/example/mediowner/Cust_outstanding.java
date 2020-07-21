355b67e8bdf3c8119509d4f16a275023aa1d2110
Parth <parthrathod5870@gmail.com>
2020-07-21 11:45:26 +0530

    Nai hona BC ye

---
+++ app/src/main/java/com/example/mediowner/Cust_outstanding.java
@@ -0,0 +1,22 @@
+package com.example.mediowner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Cust_outstanding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cust_outstanding);
+
+    }
+
+}
